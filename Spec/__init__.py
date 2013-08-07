eb3115e3ac23ab3a4d904219053383d59e0da269
Sriram Velamur <sriram.velamur@gmail.com>
2013-08-07 16:59:59 +0530

    Moved timeit call to instance method and added OrderedDict call

--- Spec/__init__.py
+++ Spec/__init__.py
@@ -4,6 +4,7 @@ import sys
 sys.dont_write_bytecode = True
 
 import timeit
+from collections import OrderedDict
 # from profile import Profile
 
 
@@ -26,6 +27,40 @@ class Spec(object):
         self._test_methods = []
         self.config = config
         self.results = {}
+
+    def call_timeit(self, method):
+        """Helper to call timeit on method and add to results"""
+        if not (method and hasattr(method.get("method_def", {}), "__call__")):
+            raise Exception("Not a method")
+        args = method.get("args")
+        method_def = method.get("method_def")
+        method_name = method.get("method_name")
+        if args:
+            print "Starting timeit on method {0}".format(method_name)
+            timer = timeit.Timer(lambda: method_def.__call__(*args))
+            method_time = timer.repeat(repeat=self.config['repeats'],
+                                       number=self.config['calls'])
+            print "Ended timeit on method {0}".format(method_name)
+        else:
+            print "Starting timeit on method {0}".format(method_name)
+            method_time = timeit.repeat(method_def,
+                                        repeat=self.config['repeats'],
+                                        number=self.config['calls'])
+            print "Ended timeit on method {0}".format(method_name)
+
+        if not self.results.get(method_name):
+            self.results[method_name] = {
+                "timeit": (len(method_time) /\
+                            sum(method_time)) *\
+                              self.config['repeats']
+            }
+        else:
+            self.results[method_name]['timeit'] = (len(method_time)\
+                                                    / sum(method_time)) *\
+                                                    self.config['repeats']
+        if self.config.get("sort"):
+            self.results = OrderedDict(sorted(self.results.keys(),
+                                              key=lambda x: x[1]))
 
     def add_method(self, test_method, args=None):
         """Add method to spec"""
@@ -52,22 +87,4 @@ class Spec(object):
         if not self._test_methods:
             raise Exception("No methods added to suite.")
         for method in self._test_methods:
-            args = method.get("args")
-            method_def = method.get("method_def")
-            method_name = method.get("method_name")
-            if args:
-                print "Starting timeit on method {0}".format(method_name)
-                method_time = timeit.repeat(method_def,
-                                            repeat=self.config['repeats'],
-                                            number=self.config['calls'])
-                print "Ended timeit on method {0}".format(method_name)
-            else:
-                print "Starting timeit on method {0}".format(method_name)
-                method_time = timeit.repeat(method_def,
-                                            repeat=self.config['repeats'],
-                                            number=self.config['calls'])
-                print "Ended timeit on method {0}".format(method_name)
-
-            self.results[method_name] = (len(method_time) / 
-                                         sum(method_time)) *\
-                                          self.config['repeats']
+            self.call_timeit(method)
